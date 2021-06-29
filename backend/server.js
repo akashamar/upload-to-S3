3d1efb3c4286c91afdce339a5ce30a75da310ccb
akashamar <akash.amar96@gmail.com>
2021-06-29 17:02:21 +0530

    first

---
+++ backend/server.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const fs = require('fs')
+const util = require('util')
+const multer = require('multer')
+const cors = require('cors');
+// const { uploadFile, getFileStream } = require('./awsdkS3.js') //manual upload to s3
+const multerS3 = require('multer-s3');
+const S3 = require('aws-sdk/clients/s3')
+require('dotenv').config()
+
+const unlinkFile = util.promisify(fs.unlink)
+const app = express()
+app.use(cors());
+
+const bucketName = process.env.AWS_BUCKET_NAME
+const region = process.env.AWS_BUCKET_REGION
+const accessKeyId = process.env.AWS_ACCESS_KEY
+const secretAccessKey = process.env.AWS_SECRET_KEY
+
+const s3 = new S3({
+  region,
+  accessKeyId,
+  secretAccessKey
+})
+
+const upload = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: bucketName,
+        key: function (req, file, cb) {
+            console.log(file);
+            cb(null, file.originalname); //use Date.now() for unique file keys
+        }
+    })
+});
+
+app.post('/images', upload.single('image'), async (req, res) => {
+  const file = req.file
+  console.log(file)
+  console.log('uploaded')
+  res.send(file)
+})
+
+app.listen(8080, () => console.log("listening on port 8080"))
---
+++ front-end/src/App.js
@@ -0,0 +1,54 @@
+import { useState } from 'react'
+import axios from 'axios'
+
+import './App.css'
+
+async function postImage({image, description}) {
+  const formData = new FormData();
+  formData.append("image", image)
+  formData.append("description", description)
+
+  const result = await axios.post('http://localhost:8080/images', formData, { headers: {'Content-Type': 'multipart/form-data'}})
+  console.log(result)
+  return result.data
+}
+
+
+function App() {
+
+  const [file, setFile] = useState()
+  const [description, setDescription] = useState("")
+  const [images, setImages] = useState([])
+
+  const submit = async event => {
+    event.preventDefault()
+    const result = await postImage({image: file, description})
+    if(result.location) {
+      alert('uploaded successfully')
+    }
+    setImages([result.location, ...images])
+  }
+
+  const fileSelected = event => {
+    const file = event.target.files[0]
+    setFile(file)
+  }
+
+  return (
+    <div className="App">
+      <form onSubmit={submit}>
+        <input onChange={fileSelected} type="file" accept="image/*"></input>
+        <input value={description} onChange={e => setDescription(e.target.value)} type="text"></input>
+        <button type="submit">Submit</button>
+      </form>
+
+      { images.map( (image, idx) => (
+        <div key={idx}>
+          <img alt='' src={image}></img>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
